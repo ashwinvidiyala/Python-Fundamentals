570906bf5bb81d4be332477cc588ad7c4a8558c2
“Ashwin <ashwinvidiyala@gmail.com>
2018-01-08 15:27:44 -0600

    Finish filter_by_type assignment

---
+++ filter_by_type.py
@@ -0,0 +1,41 @@
+sI = 45 #type int
+mI = 100
+bI = 455
+eI = 0
+spI = -23
+sS = "Rubber baby buggy bumpers" #type str
+mS = "Experience is simply the name we give our mistakes"
+bS = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+eS = ""
+aL = [1,7,4,21] #type list
+mL = [3,5,7,34,3,2,113,65,8,89]
+lL = [4,34,22,68,9,13,3,5,7,9,2,12,45,923]
+eL = []
+spL = ['name','address','phone number','social security number']
+testValues = [sI, mI, bI, eI, spI, sS, mS, bS, eS, aL, mL, lL, eL, spL]
+
+type(sI)
+isinstance(sI, int)
+
+def filterByType(value):
+    if isinstance(value, int):
+        if value >= 100:
+            print "That's a big number"
+        else:
+            print "That's a small number"
+    elif isinstance(value, str):
+        if len(value) >= 50:
+            print "Long sentence"
+        else:
+            print "Short sentence"
+    elif isinstance(value, list):
+        if len(value) >= 10:
+            print "Big list!"
+        else:
+            print "Short list."
+    else:
+        print "Empty string"
+    return
+
+for value in testValues:
+    filterByType(value)
