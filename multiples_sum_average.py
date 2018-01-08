4f16ef649190f08c7a24a927fb8ac5a7552c1d6e
“Ashwin <ashwinvidiyala@gmail.com>
2018-01-08 15:02:05 -0600

    Finish multiples_sum_average assignment

--- multiples_sum_average.py
+++ multiples_sum_average.py
@@ -1,2 +1,20 @@
 # Multiples
-# Part 1
+
+# Part 1: Print odd numbers from 1 to 1000
+for x in range(1,1000):
+    if x % 2 is not 0:
+        print x
+
+# Part 2: Print all multiples of 5 from 5 to 1,000,000
+for x in range(5,1000000):
+    if x % 5 is 0:
+        print x
+
+#Sum list & Average List
+a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for x in a:
+    sum = sum + x
+print sum
+average = sum/(len(a))
+print average
