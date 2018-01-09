453e30bf0cdf505193f01b6bc590a869f3fbda88
“Ashwin <ashwinvidiyala@gmail.com>
2018-01-09 14:50:35 -0600

    Finish names.py

--- names.py
+++ names.py
@@ -9,7 +9,7 @@ students = [
 for value in students:
     print value["first_name"], value["last_name"]
 
-#Part 2
+# Part 2
 users = {
  'Students': [
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -22,3 +22,11 @@ users = {
      {'first_name' : 'Martin', 'last_name' : 'Puryear'}
   ]
  }
+
+for key,value in users.iteritems():
+ print key
+ for val in value:
+     print "{} - {} {} - {}".format(value.index(val) + 1,
+            val["first_name"].upper(),
+            val["last_name"].upper(),
+            len(val["first_name"]+val["last_name"]))
